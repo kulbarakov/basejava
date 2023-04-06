3e4aa31e560cdaa9253c52fcc8821f56ec1fccc1
Azamat Kulbarakov <mr.kulbarakov@gmail.com>
2023-04-06 12:40:05 +0500

    Update all methods in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,52 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < this.size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (this.get(r.uuid) == null)
+            storage[this.size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                for (int j = i; j < storage.length - 1; j++)
+                    storage[j] = storage[j + 1];
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        int size = this.size();
+        if (size > 0) {
+            Resume[] resumes = new Resume[size];
+            System.arraycopy(storage, 0, resumes, 0, size);
+            return resumes;
+        }
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) return i;
+        }
+        return storage.length;
     }
 }
